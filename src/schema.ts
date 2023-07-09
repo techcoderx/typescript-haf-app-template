21b69be62652bc2980eed05035763c1c2c1ff813
techcoderx <engtianxi@outlook.com>
2023-07-09 20:50:48 +0800

    various fixes

--- src/schema.ts
+++ src/schema.ts
@@ -5,7 +5,7 @@ import { START_BLOCK, DB_VERSION, APP_CONTEXT, SCHEMA_NAME } from './constants.j
 import db from './db.js'
 import context from './context.js'
 import logger from './logger.js'
-import { FKS_TYPE } from './schema_types.js'
+import { FKS_TYPE, INDEXES_TYPE, Ordering } from './schema_types.js'
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
@@ -14,6 +14,9 @@ const HAF_TABLES: string[] = []
 
 // FK name: FKS_TYPE
 const HAF_FKS: FKS_TYPE = {}
+
+// indexes
+const INDEXES: INDEXES_TYPE = {}
 
 const schema = {
     setup: async () => {
@@ -42,6 +45,12 @@ const schema = {
         // start block
         let startBlock = Math.max(START_BLOCK-1,0)
         logger.info('Set start block to #'+(startBlock+1))
+        if (startBlock > 0) {
+            logger.info('Updating state providers to starting block...')
+            let start = new Date().getTime()
+            await db.client.query('SELECT hive.app_state_providers_update($1,$2,$3);',[0,startBlock,APP_CONTEXT])
+            logger.info('State providers updated in',(new Date().getTime()-start),'ms')
+        }
 
         // fill with initial values
         await db.client.query(`INSERT INTO ${SCHEMA_NAME}.state(last_processed_block, db_version) VALUES($1, $2);`,[startBlock,DB_VERSION])
@@ -83,6 +92,29 @@ const schema = {
                 await db.client.query(`ALTER TABLE ${HAF_FKS[fk].table} DROP CONSTRAINT IF EXISTS ${fk};`)
                 logger.info('FK',fk,'dropped in',(new Date().getTime()-start),'ms')
             }
+    },
+    indexExists: async (index_name: string): Promise<boolean> => {
+        return (await db.client.query('SELECT * FROM pg_indexes WHERE schemaname=$1 AND indexname=$2',[SCHEMA_NAME, index_name])).rowCount > 0
+    },
+    indexCreate: async () => {
+        for (let idx in INDEXES) {
+            if (await schema.indexExists(idx)) {
+                logger.info('Index',idx,'already exists, skipping')
+                continue
+            }
+            let start = new Date().getTime()
+            await db.client.query(`CREATE INDEX IF NOT EXISTS ${idx} ON ${INDEXES[idx].table_name}(${INDEXES[idx].columns.map(x => x.col_name+' '+Ordering[x.order]).join(',')});`)
+            logger.info('Index',idx,'created in',(new Date().getTime()-start),'ms')
+        }
+    },
+    indexDrop: async () => {
+        for (let idx in INDEXES) {
+            if (await schema.indexExists(idx)) {
+                let start = new Date().getTime()
+                await db.client.query(`DROP INDEX IF EXISTS ${idx} CASCADE;`)
+                logger.info('Index',idx,'dropped in',(new Date().getTime()-start),'ms')
+            }
+        }
     }
 }
 
--- src/schema_types.ts
+++ src/schema_types.ts
@@ -7,3 +7,22 @@ export type FK_TYPE = {
 export type FKS_TYPE = {
     [key: string]: FK_TYPE
 }
+
+export type INDEX_TYPE = {
+    table_name: string,
+    columns: INDEX_COL_TYPE[]
+}
+
+export type INDEX_COL_TYPE = {
+    col_name: string,
+    order: Ordering
+}
+
+export type INDEXES_TYPE = {
+    [key: string]: INDEX_TYPE
+}
+
+export enum Ordering {
+    ASC,
+    DESC
+}
